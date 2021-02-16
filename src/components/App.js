ce0da9b351a7d7906685e4ca5e0977426f112f81
Yuguo Zhao <cheerzhao77@gmail.com>
2021-02-15 19:08:57 -0600

    refactor: add Dashboard component, update App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { handleInitialData } from "../actions/shared";
 import { connect } from "react-redux";
+import Dashboard from "./Dashboard";
 
 class App extends Component {
   componentDidMount() {
@@ -10,19 +11,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
-        {Object.values(this.props.tweets).map(({ author, id }) => (
-          <p key={id}>{author}</p>
-        ))}
-        {Object.values(this.props.users).map(({ name, id }) => (
-          <h1 key={id}>{name}</h1>
-        ))}
+        {this.props.loading === true ? <p>Loading...</p> : <Dashboard />}
       </div>
     );
   }
 }
 
-export default connect((state) => ({
-  tweets: state.tweets,
-  users: state.users,
-}))(App);
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
